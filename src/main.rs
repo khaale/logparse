6e1ba32b64cc4ce14e352e8b4c3b6e5006538ee9
Aleksander Khanteev <khaale@yandex.ru>
2017-04-30 01:04:20 +0400

    Use builder in main

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,25 @@ extern crate chrono;
 extern crate regex;
 
 use reader::*;
-
+use model::*;
 use std::io::BufReader;
 use std::fs::File;
 
 fn main() {
-    let f = File::open(r"C:\Work\Projects\logparser\log.txt").unwrap();
+
+    let f = File::open(r"C:\Users\Aleksander\Documents\projects\logparse\log.txt").unwrap();
     let r = BufReader::new(f);
 
-    let mut r = EventReader::new(r);
+    let mut reader = EventReader::new(r);
+    let mut builder = Builder::new();
 
-    while let Some(e) = r.next() {
+    while let Some(e) = reader.next() {
         match e {
-            Event::PackageStarted(e) => println!("{}", e.value),
-            Event::ContainerFinished(e) => println!("{}", e.value),
-            Event::PreExecuteTask(e) => println!("{}", e.value),
-            Event::PostExecuteTask(e) => println!("{}", e.value)
+            Event::PackageStarted(e) => builder.start_package(&e),
+            Event::ContainerFinished(e) => builder.container_name(&e),
+            Event::PreExecuteTask(e) => builder.pre_task(&e),
+            Event::PostExecuteTask(e) => builder.post_task(&e)
         }
     }
+    println!("{:?}", builder.packages)
 }
