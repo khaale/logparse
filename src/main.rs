2646341b0ca037ba78b51ff4a8c578ec4cd10768
Aleksander Khanteev <khaale@yandex.ru>
2017-05-01 15:10:30 +0400

    Add results processing

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ mod model;
 #[macro_use] extern crate lazy_static;
 extern crate chrono;
 extern crate regex;
+extern crate itertools;
 
 use reader::*;
 use model::*;
 use std::io::BufReader;
 use std::fs::File;
+use itertools::Itertools;
 
 fn main() {
 
@@ -26,5 +28,30 @@ fn main() {
             Event::PostExecuteTask(e) => builder.post_task(&e)
         }
     }
-    println!("{:?}", builder.packages)
+    println!("{:?}", builder.packages);
+
+    let result = builder.packages.iter()
+        .flat_map(|p| {
+            get_leaf_tasks(&p.tasks)
+                .iter()
+                .map(|t| (
+                    (&p.package_name, &p.container_name, &t.name),
+                    t.end_time.signed_duration_since(t.start_time).num_milliseconds()
+                ))
+                .collect::<Vec<_>>()
+        })
+        //.collect_vec().iter()
+        //.group_by(|x| x.0)
+        .collect_vec();
+
+    println!("{:?}", result);
+}
+
+fn get_leaf_tasks(tasks : &Vec<Task>) -> Vec<&Task> {
+        tasks.iter().flat_map(|t|
+            if t.tasks.is_empty() {
+                vec![t]
+            } else {
+                get_leaf_tasks(&t.tasks)
+            }).collect::<Vec<_>>()
 }
